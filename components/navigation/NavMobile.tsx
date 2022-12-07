cbda8761d370bd472d7480cb6584d938b76e3cdf
kenneth vega <kenneth.trinidad.vega@gmail.com>
2022-12-07 13:02:07 +0800

    mobile bug

--- components/navigation/NavMobile.tsx
+++ components/navigation/NavMobile.tsx
@@ -53,70 +53,70 @@ const NavMobile = () => {
             button-name="mobile hamburger navigation menu"
           />
         </button>
-        <nav>
-          <ul
-            id="primary-navigation"
-            className={`${
-              isOpen ? "active" : ""
-            } primary-navigation  flex justify-center items-center gap-5 text-sm`}
-          >
-            <li onClick={() => setIsOpen(false)}>
-              <Link href="/#projects" aria-label="kenneth vega projects">
-                Projects
-              </Link>
-            </li>
-
-            <li onClick={() => setIsOpen(false)}>
-              <Link href="/#about" aria-label="kenneth vega about">
-                About
-              </Link>
-            </li>
-
-            <li
-              onClick={handleModal}
-              className="px-2 py-1 cursor-pointer rounded-md hover:bg-gray-200 transition-all duration-300"
-            >
-              Contact
-            </li>
-            <li>
-              <a
-                href="https://firebasestorage.googleapis.com/v0/b/my-portfolio-d50af.appspot.com/o/Kenneth%20Vega%20Resume%20(5).pdf?alt=media&token=52e4adca-dc6c-44c2-ae57-777c956adc15"
-                target="_blank"
-                rel="noreferrer"
-                className="flex justify-center gap-2 items-center"
-              >
-                Resume <TbExternalLink />
-              </a>
-            </li>
-
-            <div className="hidden md:flex gap-8 text-3xl ">
-              <Tippy content="Github profile">
-                <a
-                  href="https://github.com/kennethvega"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="cursor-pointer"
-                  aria-label="github link"
-                >
-                  <AiFillGithub />
-                </a>
-              </Tippy>
-              <Tippy content="Linkedin profile">
-                <a
-                  href="https://www.linkedin.com/in/kenneth-vega-5bb9b3237/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="cursor-pointer"
-                  aria-label="linkedin link"
-                >
-                  <AiFillLinkedin />
-                </a>
-              </Tippy>
-            </div>
-          </ul>
-        </nav>
         {isOpen && <Overlay handleClose={() => setIsOpen(false)} />}
       </header>
+      <nav>
+        <ul
+          id="primary-navigation"
+          className={`${
+            isOpen ? "active" : ""
+          } primary-navigation  flex justify-center items-center gap-5 text-sm`}
+        >
+          <li onClick={() => setIsOpen(false)}>
+            <Link href="/#projects" aria-label="kenneth vega projects">
+              Projects
+            </Link>
+          </li>
+
+          <li onClick={() => setIsOpen(false)}>
+            <Link href="/#about" aria-label="kenneth vega about">
+              About
+            </Link>
+          </li>
+
+          <li
+            onClick={handleModal}
+            className="px-2 py-1 cursor-pointer rounded-md hover:bg-gray-200 transition-all duration-300"
+          >
+            Contact
+          </li>
+          <li>
+            <a
+              href="https://firebasestorage.googleapis.com/v0/b/my-portfolio-d50af.appspot.com/o/Kenneth%20Vega%20Resume%20(5).pdf?alt=media&token=52e4adca-dc6c-44c2-ae57-777c956adc15"
+              target="_blank"
+              rel="noreferrer"
+              className="flex justify-center gap-2 items-center"
+            >
+              Resume <TbExternalLink />
+            </a>
+          </li>
+
+          <div className="hidden md:flex gap-8 text-3xl ">
+            <Tippy content="Github profile">
+              <a
+                href="https://github.com/kennethvega"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="cursor-pointer"
+                aria-label="github link"
+              >
+                <AiFillGithub />
+              </a>
+            </Tippy>
+            <Tippy content="Linkedin profile">
+              <a
+                href="https://www.linkedin.com/in/kenneth-vega-5bb9b3237/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="cursor-pointer"
+                aria-label="linkedin link"
+              >
+                <AiFillLinkedin />
+              </a>
+            </Tippy>
+          </div>
+        </ul>
+      </nav>
     </motion.div>
   );
 };
