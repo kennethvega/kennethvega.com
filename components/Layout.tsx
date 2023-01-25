63e810d6cc8f05055afbbe6ac87f6498b7f35e2f
kenneth vega <kenneth.trinidad.vega@gmail.com>
2023-01-25 22:32:15 +0800

    Display: fallback to fix font style not applying sometimes

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -6,6 +6,7 @@ import { Poppins } from "@next/font/google";
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700", "900"],
+  display: "fallback",
 });
 
 type LayoutProps = {
